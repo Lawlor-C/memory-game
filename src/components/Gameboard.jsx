fbef7e4b97e4211bd5c7beef4bd152606f57902f
Chris Lawlor <chris.d.lawlor@gmail.com>
2025-03-19 18:57:03 +0000

    cards now flip when clicked and don't move around mid-game

--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -1,48 +1,58 @@
 import { useState } from "react";
 import { cardsArray } from "../data/cardData";
 import Card from "./Card";
-import "../styles/Gameboard.css"
+import "../styles/Gameboard.css";
 
+//whole Gameboard
 export default function Gameboard() {
-    
-    const selectedSet = "test";
-    const { cards, backImage } = cardsArray[selectedSet];
-    const doubleSelectedSet = [...cards, ...cards];
-    const shuffleArray = (array) => array.sort(() => Math.random() - 0.5);
-    const shuffledCards = shuffleArray(doubleSelectedSet);
-    
-    const initialFlipState = shuffledCards.map((card, index) => ({
-        index: index,
-        isFlipped: false,
-    }));
-    const [flipState, setFlipState] = useState(initialFlipState);
+  const selectedSet = "test"; //Hardcoding this as test for now
+  const { cards, backImage } = cardsArray[selectedSet];
 
-    const handleCardClick = (index) => {
-        console.log(`Clicked card at index ${index}`);
-        setFlipState(prevState =>
-            prevState.map((card, i) =>
-                i === index ? { ...card, isFlipped: !card.isFlipped } : card
-            )
-        );
-    };
-    
-      
+  //Need to duplicate the cards so can be paired (trial and error with uniqueness)
+  const doubleSelectedSet = [...cards, ...cards].map((card, index) => ({
+    ...card,
+    uniqueId: `${card.id}-${index}`,
+  }));
+  //Randomise the double-deck (but create them only once)
+  const shuffleArray = (array) => array.sort(() => Math.random() - 0.5);
+  const [shuffledCards] = useState(() => shuffleArray(doubleSelectedSet));
 
-    
-    
-    return (
-        <div className="gameboard">
-        {shuffledCards.map((card, index) => (
-        <Card
-         key={`${card.id}-${index}`}
-        content={card.content}
-        image={card.image}
-        backImage={backImage}
-        isFlipped={flipState[index].isFlipped}
-        isMatched={false}
-        handleCardClick={() => handleCardClick(index)}
-        />
-        ))}
-        </div>
+  //Render the cards, unflipped (ie face-down)
+  const initialFlipState = shuffledCards.map((card) => ({
+    uniqueId: card.uniqueId,
+    isFlipped: false,
+  }));
+  const [flipState, setFlipState] = useState(initialFlipState);
+
+  //Flip card when clicked (should not affect other cards)
+  const handleCardClick = (uniqueId) => {
+    setFlipState((prevState) =>
+      prevState.map((card) =>
+        card.uniqueId === uniqueId
+          ? { ...card, isFlipped: !card.isFlipped }
+          : card
+      )
     );
+  };
+
+  return (
+    <div className="gameboard">
+      {shuffledCards.map((card) => {
+        const flippedState = flipState.find((state) => state.uniqueId === card.uniqueId);
+  
+        return (
+          <Card
+            key={card.uniqueId} 
+            content={card.content}
+            image={card.image}
+            backImage={backImage}
+            isFlipped={flippedState.isFlipped}
+            isMatched={false}
+            handleCardClick={() => handleCardClick(card.uniqueId)}
+          />
+        );
+      })}
+    </div>
+  );
+  
 }
