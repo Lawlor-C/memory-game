5bf547f11b850bed932c18a47bfd9a54509d097d
Chris Lawlor <chris.d.lawlor@gmail.com>
2025-03-20 16:36:01 +0000

    Matching now works, timeout set for non-matches. Matched cards remain clickable

--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -1,13 +1,18 @@
 import { useState } from "react";
+import { useEffect } from "react";
 import 'animate.css';
 import { cardsArray } from "../data/cardData";
 import Card from "./Card";
 import "../styles/Gameboard.css";
 
+
+
 //whole Gameboard - will streamline into utils once all functioning correctly
 export default function Gameboard() {
   const selectedSet = "test"; //Hardcoding this as test for now
   const { cards, backImage } = cardsArray[selectedSet];
+
+
 
   //Need to duplicate the cards so can be paired (trial and error with uniqueness)
   const doubleSelectedSet = [...cards, ...cards].map((card, index) => ({
@@ -18,6 +23,7 @@ export default function Gameboard() {
   const shuffleArray = (array) => array.sort(() => Math.random() - 0.5);
   const [shuffledCards] = useState(() => shuffleArray(doubleSelectedSet));
 
+
   //Render the cards, unflipped (ie face-down)
   const initialFlipState = shuffledCards.map((card) => ({
     uniqueId: card.uniqueId,
@@ -26,9 +32,13 @@ export default function Gameboard() {
   const [flippedCards, setFlippedCards] = useState([]);
   const [flipState, setFlipState] = useState(initialFlipState);
 
-  //Flip card when clicked (should not affect other cards - updated to put clicked cards into a new array, max 2)
+
+
+  //Interact with cards via click
   const handleCardClick = (uniqueId) => {
     console.log(`Clicked card with ID ${uniqueId}`);
+
+    //Add clicked cards (max 2) into temp array to prepare to check for match
     setFlippedCards((prev) => {
         if (prev.length < 2) {
             return [...prev, uniqueId];        
@@ -36,6 +46,8 @@ export default function Gameboard() {
         return prev;
     });
 
+
+    //Renders the flipped version of the card for user to see
     setFlipState((prevState) =>
       prevState.map((card) =>
         card.uniqueId === uniqueId
@@ -44,6 +56,40 @@ export default function Gameboard() {
       )
     );
   };
+
+  //Effect to kick in once the temp array holds two cards, compare content for match
+  useEffect(() => {
+    if (flippedCards.length === 2) {
+        const [firstCard, secondCard] = flippedCards;
+        const firstCardContent = shuffledCards.find(card => card.uniqueId === firstCard).content;
+        const secondCardContent = shuffledCards.find(card => card.uniqueId === secondCard).content;
+
+        if (firstCardContent === secondCardContent) {
+            console.log("It's a Match!");
+            setFlipState((prevState) =>
+            prevState.map((card) =>
+            flippedCards.includes(card.uniqueId)
+        ? {...card, isMatched: true}
+        : card
+        )
+    );
+        } else {
+            console.log("Not a Match!");
+            setTimeout(() => {
+                setFlipState((prevState) =>
+                prevState.map((card) =>
+                flippedCards.includes(card.uniqueId)
+            ? {...card, isFlipped: false}
+            : card
+        )
+    );
+            }, 1000);
+        }
+        setFlippedCards([]);
+    }
+  }, [flippedCards, shuffledCards]);
+
+
 
   return (
     <div className="gameboard">
